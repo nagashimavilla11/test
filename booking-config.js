9254de571a32b078a168358cbc066bd0276fa7d1
nagashimavilla11 <nagashima.villa.11@gmail.com>
2026-08-29 13:06:56 +0900

    Update booking-config.js

--- booking-config.js
+++ booking-config.js
@@ -1,2 +1,2 @@
 /* Google Apps Scriptを公開後、下記の空文字内へウェブアプリURLを貼り付けます。 */
-window.NV11_BOOKING_APP_URL='';
+window.NV11_BOOKING_APP_URL='https://script.google.com/macros/s/AKfycbxNqapqTLu-HDpoEVVsLsbQmxr3vdzclePHPb6-tbaVRfmaKRtfKzg_bSKIdb9oMdPZ/exec';
